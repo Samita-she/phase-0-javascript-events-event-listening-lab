898af682b7b233f80f85dbbfda09e9bf700274e7
Sheila Samita <samitasheila1@gmail.com>
2025-03-01 12:38:31 +0300

    events listening

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
 }
+function addingEventListener() {
+    const input = document.getElementById('button');  // Select the button
+    input.addEventListener('click', function () {      // Add event listener
+        alert('I was clicked!');                        // Show alert on click
+    });
+}
+
